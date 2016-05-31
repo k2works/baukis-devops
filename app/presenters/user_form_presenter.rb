365024864220ea7b7d1b4b701135ed7f96ff6abd
k2works <kakimomokuri@gmail.com>
2016-05-31 17:23:45 +0900

    Capybara

--- app/presenters/user_form_presenter.rb
+++ app/presenters/user_form_presenter.rb
@@ -1,5 +1,5 @@
 class UserFormPresenter < FormPresenter
-  def password_field_black(name, label_text, options = {})
+  def password_field_block(name, label_text, options = {})
     if object.new_record?
       super(name, label_text, options)
     else
---
+++ spec/support/features_spec_helper.rb
@@ -0,0 +1,15 @@
+module FeaturesSpecHelper
+  def switch_namespace(namespace)
+    config = Rails.application.config.baukis
+    Capybara.app_host = 'http://' + config[namespace][:host]
+  end
+
+  def login_as_staff_member(staff_memeber, password = 'pw')
+    visit staff_login_path
+    within('#login-form') do
+      fill_in 'メールアドレス', with: staff_memeber.email
+      fill_in 'パスワード', with: password
+      click_button 'ログイン'
+    end
+  end
+end
